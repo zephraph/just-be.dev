d94bd5be88d9055604f14e7a871fdcfea2c7573b
Justin Bennett <git@just-be.dev>
2023-01-21 22:16:57 -0500

    Add social links

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,14 +1,37 @@
-<nav class="flex justify-between items-center pt-3 pb-16 md:pt-4">
+<script>
+	import GitHubIcon from './icons/GitHubIcon.svelte';
+	import LinkedInIcon from './icons/LinkedInIcon.svelte';
+	import MastodonIcon from './icons/MastodonIcon.svelte';
+	import TwitterIcon from './icons/TwitterIcon.svelte';
+</script>
+
+<nav class="flex justify-between items-center pt-3 pb-16 md:pt-4 text-gray-400 ">
 	<a
-		class="animated-header bastia whitespace-nowrap text-3xl items-center mr-2 text-gray-400 hover:text-gray-800 ease-out duration-300 cursor-pointer"
+		class="animated-header bastia whitespace-nowrap text-3xl items-center mr-2 hover:text-gray-800 ease-out duration-300 cursor-pointer"
 		href="/"
 	>
 		<span>just</span><span class="fade-in">in</span>
 		<span class="last"><span>be</span><span class="fade-in">nnett</span></span>
 	</a>
+	<div class="flex [&>*>*]:h-7 space-x-2 social-icons">
+		<a href="https://pkm.social/@just_be" class="hover:[&>*]:text-[#3088d4]"><MastodonIcon /></a>
+		<a href="https://github.com/zephraph" class="hover:[&>*]:text-[#181717]"><GitHubIcon /></a>
+		<a href="https://twitter.com/zephraph" class="hover:[&>*]:text-[#1da1f2]"><TwitterIcon /></a>
+		<a href="https://www.linkedin.com/in/just-be/" class="hover:[&>*]:text-[#0077b5]"
+			><LinkedInIcon /></a
+		>
+	</div>
 </nav>
 
 <style>
+	.social-icons > *,
+	.social-icons :global(svg) {
+		transition: 0.15s all ease-in-out;
+	}
+	.social-icons > *:hover {
+		transform: scale(1.2);
+	}
+
 	.fade-in {
 		opacity: 0;
 	}
--- src/routes/+page.md
+++ src/routes/+page.md
@@ -1,5 +1,3 @@
-I'm a software engineer based in Brooklyn, NY. I work for [Oxide Computer Company](https://oxide.computer). I'm rewriting my site (sorry for the broken links; they'll be fixed soon) so here's a few places you can find me until I'm done. 
+I'm a software engineer at [Oxide Computer Company](https://oxide.computer) working remotely from Brooklyn, NY. Previously at [Artsy](https://www.artsy.net). I co-host the [devtools.fm](https://devtools.fm) podcast.
 
-- [@zephraph](https://twitter.com/zephraph) on Twitter
-- [@just_be@pkm.social](https://pkm.social/@just_be) on Mastodon
-- [zephraph](https://github.com/zephraph) on GitHub
+Want to chat? Shoot me an email at hi@just-be.dev.
