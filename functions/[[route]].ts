b1dfe982b9e116ad80d456b2f495e0aee6a234f1
Justin Bennett <oss@just-be.dev>
2023-08-25 11:09:45 -0400

    Flatten routes down to a single file

--- functions/[[route]].ts
+++ functions/[[route]].ts
@@ -1,6 +1,10 @@
 import { Hono } from 'hono'
 const app = new Hono()
 
+app.get('/api/hello', (c) => {
+  return c.html('<h1>Hello world</h1>')
+})
+
 app.get('/*', (c) => {
   console.log(c.req.url)
   return fetch(`https://notes.just-be.dev/${c.req.url}`)
