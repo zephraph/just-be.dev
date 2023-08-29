b2a2a327df9e94d7e55d10b1d9a055f5518ab1cf
Justin Bennett <git@just-be.dev>
2023-08-29 13:31:34 -0400

    WIP

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,23 @@ app.get("/_obsidian/:prefix/*", async (c) => {
   return fetch(`https://publish-${c.req.param("prefix")}.obsidian.md/${path}`);
 });
 
+// Redirects from my old posts
+app.get("/posts/:slug", async (c) => {
+  const slug = c.req.param("slug");
+  const redirectMap = {
+    "export-import-indexeddb": "/-/01FCSNVWG0WNCZTS9JCMGQE3KS",
+  };
+  // http://127.0.0.1:8787/-/01FCSNVWG0WNCZTS9JCMGQE3KS
+  // http://127.0.0.1:8787/-/01FCSNVWG0WNCZTS9JCMGQE3KS
+
+  const redirect = redirectMap[slug as keyof typeof redirectMap];
+  if (redirect) {
+    return c.redirect(redirect, 301);
+  } else {
+    return c.notFound();
+  }
+});
+
 app.get("/*", async (c) => {
   return fetch(`https://notes.just-be.dev/${c.req.url}`).then((res) => {
     const { origin } = new URL(c.req.url);
