a0da526b9d700cf1dadc870b567e56381ebae933
Justin Bennett <git@just-be.dev>
2024-08-04 12:17:26 -0400

    Add tailwind
    The astro config has an unrelated chagne of important and integrating normalize layouts. That's supposed to be for a separate commit, but I'm using GitBulter and I don't have a way of breaking this hunk down further unfortunately. It's fine.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import remarkObsidian from "remark-obsidian";
+import { normalizeLayouts } from "./plugins/normalize-layouts.remark.mjs";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
   markdown: {
-    remarkPlugins: [remarkObsidian],
+    remarkPlugins: [normalizeLayouts, remarkObsidian]
   },
+  integrations: [tailwind()]
 });
