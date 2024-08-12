491373abf68cbe402bc904947ef470aa94edfa6d
Justin Bennett <git@just-be.dev>
2024-08-12 10:46:05 -0400

    test commit: is sentry the problem?

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,18 +6,19 @@ import { defineMiddleware } from "astro:middleware";
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware(
   ({ request, locals: { runtime: context } }, next) => {
-    if (process.env.IS_BUILD) {
-      return next();
-    }
-    return Sentry.wrapRequestHandler(
-      {
-        options: {
-          dsn: context.env.SENTRY_DSN,
-        },
-        request,
-        context: context.ctx,
-      },
-      () => next()
-    );
+    return next();
+    // if (process.env.IS_BUILD) {
+    //   return next();
+    // }
+    // return Sentry.wrapRequestHandler(
+    //   {
+    //     options: {
+    //       dsn: context.env.SENTRY_DSN,
+    //     },
+    //     request,
+    //     context: context.ctx,
+    //   },
+    //   () => next()
+    // );
   }
 );
