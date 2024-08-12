812141f157521a3f58462d3dbf815d6a3a2b6937
Justin Bennett <git@just-be.dev>
2024-08-11 21:25:07 -0400

    Add sentry to figure out the 500

--- astro.config.ts
+++ astro.config.ts
@@ -3,22 +3,23 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
-
 import { normalizeFrontmatter } from "./packages/my-remark";
 import { remarkObsidian } from "./packages/remark-obsidian";
 import mdRenderer from "./packages/astro-md";
+
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   markdown: {
-    remarkPlugins: [normalizeFrontmatter, remarkObsidian],
+    remarkPlugins: [normalizeFrontmatter, remarkObsidian]
   },
-  integrations: [mdRenderer(), tailwind()],
+  integrations: [mdRenderer(), tailwind(), sentry()],
   adapter: cloudflare({
     imageService: "cloudflare",
     platformProxy: {
-      enabled: true,
-    },
-  }),
+      enabled: true
+    }
+  })
 });
