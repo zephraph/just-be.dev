7f482e92b50605659dc7af2a04fa3de4eeba2daa
Justin Bennett <git@just-be.dev>
2024-08-12 10:55:55 -0400

    test commit: Is it layout imports?

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,19 +6,18 @@ import { defineMiddleware } from "astro:middleware";
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware(
   ({ request, locals: { runtime: context } }, next) => {
-    return next();
-    // if (process.env.IS_BUILD) {
-    //   return next();
-    // }
-    // return Sentry.wrapRequestHandler(
-    //   {
-    //     options: {
-    //       dsn: context.env.SENTRY_DSN,
-    //     },
-    //     request,
-    //     context: context.ctx,
-    //   },
-    //   () => next()
-    // );
+    if (process.env.IS_BUILD) {
+      return next();
+    }
+    return Sentry.wrapRequestHandler(
+      {
+        options: {
+          dsn: context.env.SENTRY_DSN,
+        },
+        request,
+        context: context.ctx,
+      },
+      () => next()
+    );
   }
 );
