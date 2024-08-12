8cdbdbf40ed11aa983b392e543e117a7433cd6db
Justin Bennett <git@just-be.dev>
2024-08-12 17:35:26 -0400

    Note CF build failure; improve sentry vite config

--- astro.config.ts
+++ astro.config.ts
@@ -16,6 +16,10 @@ process.env.IS_BUILD = "true";
 
 // https://astro.build/config
 export default defineConfig({
+  /**
+   * Note: I'd like this to be hybrid, but when that's enabled cf deployments give me a 500 error
+   * @see https://just-be.sentry.io/share/issue/ab09f9239b3f45d1b27a222743afd6aa/
+   */
   output: "server",
   markdown: {
     remarkPlugins: [normalizeFrontmatter, remarkObsidian],
@@ -36,9 +40,15 @@ export default defineConfig({
     },
     plugins: [
       sentryVitePlugin({
+        /**
+         * CF_PAGES set by Cloudflare at build time
+         * @see https://developers.cloudflare.com/pages/configuration/build-configuration/#environment-variables
+         */
+        disable: !process.env.CF_PAGES,
         authToken: process.env.SENTRY_AUTH_TOKEN,
         org: "just-be",
         project: "just-be-dev",
+        telemetry: false,
       }),
     ],
   },
