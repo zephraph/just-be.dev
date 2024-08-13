f41f3282ee1cdf1e631b80110952d90914aa018c
Justin Bennett <git@just-be.dev>
2024-08-12 21:09:45 -0400

    Package up cloudflare sentry into an astro integration (#13)

--- astro.config.ts
+++ astro.config.ts
@@ -6,13 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 import { normalizeFrontmatter } from "./packages/my-remark";
 import { remarkObsidian } from "./packages/remark-obsidian";
 import mdRenderer from "./packages/astro-md";
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-
-/**
- * This is a little hack to be able to progrmmatically detect
- * if we're pre-rendering or in SSR mode.
- */
-process.env.IS_BUILD = "true";
+import astroCloudflareSentry from "./packages/astro-cf-sentry";
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,32 +18,19 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [normalizeFrontmatter, remarkObsidian],
   },
-  integrations: [mdRenderer(), tailwind()],
+  integrations: [
+    mdRenderer(),
+    tailwind(),
+    astroCloudflareSentry({
+      org: "just-be",
+      project: "just-be-dev",
+      telemetry: false,
+    }),
+  ],
   adapter: cloudflare({
     imageService: "cloudflare",
     platformProxy: {
       enabled: true,
     },
   }),
-  vite: {
-    build: {
-      sourcemap: true,
-    },
-    ssr: {
-      external: ["node:async_hooks"],
-    },
-    plugins: [
-      sentryVitePlugin({
-        /**
-         * CF_PAGES set by Cloudflare at build time
-         * @see https://developers.cloudflare.com/pages/configuration/build-configuration/#environment-variables
-         */
-        disable: !process.env.CF_PAGES,
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-        org: "just-be",
-        project: "just-be-dev",
-        telemetry: false,
-      }),
-    ],
-  },
 });
