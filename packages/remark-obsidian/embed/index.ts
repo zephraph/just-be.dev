6f45f0358dd8a0b4e295ee3736212545147978ad
Justin Bennett <git@just-be.dev>
2024-08-27 23:10:25 -0400

    what the fuck

--- packages/remark-obsidian/embed/index.ts
+++ packages/remark-obsidian/embed/index.ts
@@ -24,29 +24,3 @@ export const embedPlugin = (options: EmbedPluginOptions): RemarkPlugin =>
   };
 
 export default embedPlugin;
-import type { RemarkPlugin } from "@astrojs/markdown-remark";
-import { syntax } from "./syntax";
-import { fromMarkdown } from "./mdast";
-
-export { syntax } from "./syntax";
-export { html } from "./html";
-export { fromMarkdown } from "./mdast";
-
-interface EmbedPluginOptions {
-  unresolvedLinks: Set<string>;
-}
-
-export const embedPlugin = (options: EmbedPluginOptions): RemarkPlugin =>
-  function () {
-    // TODO: Fix this type
-    const data = this.data() as any;
-
-    data.micromarkExtensions = (data.micromarkExtensions || []).concat(
-      syntax()
-    );
-    data.fromMarkdownExtensions = (data.fromMarkdownExtensions || []).concat(
-      fromMarkdown(options)
-    );
-  };
-
-export default embedPlugin;
--- packages/remark-obsidian/embed/syntax/utils.ts
+++ packages/remark-obsidian/embed/syntax/utils.ts
@@ -24,29 +24,3 @@ export const embedCode = {
   headingMarker,
   headingOrBlockStart,
 };
-import { codifyString } from "../../parser-utils";
-
-import { internalLinkCode } from "../../internal-link/syntax/utils";
-
-const { blockMarker, headingMarker, headingOrBlockStart, aliasMarker } =
-  internalLinkCode;
-
-export const embedCode = {
-  start: codifyString("![["),
-  end: codifyString("]]"),
-
-  extension: codifyString("."),
-
-  dimension: codifyString("|"),
-  dimensionSeparator: codifyString("x"),
-
-  pdfParams: codifyString("#"),
-
-  pdfParamSeparator: codifyString("&"),
-  pdfKVSeparator: codifyString("="),
-
-  aliasMarker,
-  blockMarker,
-  headingMarker,
-  headingOrBlockStart,
-};
