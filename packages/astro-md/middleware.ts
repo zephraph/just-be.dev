07cbabb06bfd4527286dac8c6b2e3670f894356e
Justin Bennett <git@just-be.dev>
2024-09-12 13:59:17 -0400

    Replace all URLs with their canonical version

--- packages/astro-md/middleware.ts
+++ packages/astro-md/middleware.ts
@@ -6,15 +6,8 @@ import { normalizeFrontmatter } from "../my-remark";
 import { remarkObsidian } from "../remark-obsidian";
 import remarkFrontmatter from "remark-frontmatter";
 
-function isULID(str: string) {
-  // ULID regex to validate the format
-  const ulidRegex = /^[0-9A-HJKMNP-TV-Z]{26}$/i;
-  return ulidRegex.test(str);
-}
-
 export const onRequest = defineMiddleware(async (context, next) => {
   const fileResolver = async (path: string) => {
-    if (isULID(path)) return path;
     return (await context.locals.runtime.env.KV_MAPPINGS.get(path)) ?? path;
   };
   const processor = await createMarkdownProcessor({
