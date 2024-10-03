e220a58a19c14e21a9333913245183bf6eba79d0
Justin Bennett <git@just-be.dev>
2024-10-03 18:55:35 -0400

    ignore odd ts failure

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  // @ts-ignore This is failing for astro check, but not otherwise causing issues.
   plugins: [tsconfigPaths()],
   test: {
     // Add any additional test configurations here if needed
