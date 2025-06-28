17e3f45b691b1d2c1739f9f2f3c5d394c25bc3c3
Justin Bennett <git@just-be.dev>
2025-06-28 15:18:04 -0400

    Upgrade to Astro 5

---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="@cloudflare/workers-types" />
+
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+type Renderer = import("@just-be/astro-md").Renderer;
+
+declare namespace App {
+  interface Locals extends Runtime, Renderer {}
+}
