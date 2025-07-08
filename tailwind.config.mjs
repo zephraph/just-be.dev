a5d12861e86ae97fe0ea8ee4aee97cc548ce324d
Justin Bennett <git@just-be.dev>
2025-07-08 00:14:39 -0400

    Upgrade to tailwind v5 (#115)
    * Upgrade to tailwind v5
    
    * Add tailwind reference to fix @apply breaking build

--- tailwind.config.mjs
+++
@@ -1,7 +0,0 @@
-import typography from "@tailwindcss/typography";
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [typography],
-};
